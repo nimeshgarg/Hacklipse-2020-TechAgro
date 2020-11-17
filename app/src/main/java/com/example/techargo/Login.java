defb41286145aa12c7cd64f3f5ac235614f40bc9
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2020-11-17 23:57:27 +0530

    init

--- app/src/main/java/com/example/techargo/Login.java
+++ app/src/main/java/com/example/techargo/Login.java
@@ -3,8 +3,11 @@ package com.example.techargo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

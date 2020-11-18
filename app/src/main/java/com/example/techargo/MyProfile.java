2efdd44e085d778f0d22748fa52651e4301623a1
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2020-11-18 12:24:07 +0530

    Login is Complete(Registration is added) MainPage is Complete Sign-out is added to profile page

--- app/src/main/java/com/example/techargo/MyProfile.java
+++ app/src/main/java/com/example/techargo/MyProfile.java
@@ -2,13 +2,23 @@ package com.example.techargo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MyProfile extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
+        Button btn_logout = findViewById(R.id.btn_out);
+        mAuth = FirebaseAuth.getInstance();
+        btn_logout.setOnClickListener(v -> {
+            mAuth.signOut();
+            startActivity(new Intent(getApplicationContext(),Login.class));
+        });
     }
 }

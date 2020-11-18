8b84c1acbadc3aa90a894354ad04bb3935a1fa85
deepankarvarma <satwikdpshrit@gmail.com>
2020-11-18 10:18:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/techargo/My_Area.java
@@ -0,0 +1,14 @@
+package com.example.techargo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class My_Area extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my__area);
+    }
+}
---
+++ app/src/main/java/com/example/techargo/My_profile.java
@@ -0,0 +1,14 @@
+package com.example.techargo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class My_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}

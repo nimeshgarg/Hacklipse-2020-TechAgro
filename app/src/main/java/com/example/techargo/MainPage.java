7cc8037e71fea6db2c1f0b124f7053f3906c62c1
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2020-11-18 11:16:20 +0530

    Login is Complete MainPage is Complete

--- app/src/main/java/com/example/techargo/MainPage.java
+++ app/src/main/java/com/example/techargo/MainPage.java
@@ -1,15 +1,25 @@
 package com.example.techargo;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainPage extends AppCompatActivity {
-
-
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+        Button btn_area = findViewById(R.id.btn_area);
+        Button btn_field = findViewById(R.id.btn_field);
+        Button btn_profile = findViewById(R.id.btn_profile);
+        btn_profile.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),MyProfile.class)));
+        btn_field.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),MyField.class)));
+        btn_area.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), MyArea.class)));
     }
 }

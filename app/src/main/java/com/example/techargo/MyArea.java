e293ee08945435209e33a861e4936553c644b909
deepankarvarma <satwikdpshrit@gmail.com>
2020-11-20 01:26:02 +0530

    Added price,particular crop in my area In MyArea Introduced AreaInfo with elements

--- app/src/main/java/com/example/techargo/MyArea.java
+++ app/src/main/java/com/example/techargo/MyArea.java
@@ -2,7 +2,9 @@ package com.example.techargo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MyArea extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MyArea extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myarea);
+        Button btn_area_check = findViewById(R.id.btn_area_check);
+        btn_area_check.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),AreaInfo.class)));
     }
 }

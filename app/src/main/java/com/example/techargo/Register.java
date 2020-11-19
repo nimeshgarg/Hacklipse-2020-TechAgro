9dff3aa4df3e48d68e4e35ffa9b74d53ad8d72f9
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2020-11-19 11:05:31 +0530

    Login is Complete(Registration is added) MainPage is Complete Sign-out is added to profile page Registration is Complete

--- app/src/main/java/com/example/techargo/Register.java
+++ app/src/main/java/com/example/techargo/Register.java
@@ -1,25 +1,26 @@
 package com.example.techargo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Source;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class Register extends AppCompatActivity {
@@ -28,6 +29,9 @@ public class Register extends AppCompatActivity {
      private FirebaseFirestore firebase;
      private List<String> state_list,city_list,area_list,crop_list;
      private ArrayAdapter<String> adapter_state,adapter_city,adapter_area,adapter_crop;
+     private Button btn_submit;
+     private EditText txt_email,txt_name,txt_pass;
+     private ProgressBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +40,13 @@ public class Register extends AppCompatActivity {
         spn_city = findViewById(R.id.spn_city_register);
         spn_area = findViewById(R.id.spn_area_register);
         spn_crop = findViewById(R.id.spn_crop_register);
+        btn_submit = findViewById(R.id.btn_register);
+        txt_email = findViewById(R.id.txt_register_email);
+        txt_name = findViewById(R.id.txt_register_name);
+        txt_pass = findViewById(R.id.txt_register_password);
+        bar = findViewById(R.id.bar_register);
         firebase = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         firebase.collection("state").get().addOnCompleteListener(task -> {
             state_list = new ArrayList<>();
             state_list.add("Select State");
@@ -163,6 +173,95 @@ public class Register extends AppCompatActivity {
 
             }
         });
+        btn_submit.setOnClickListener(v -> {
+            btn_submit.setEnabled(false);
+            btn_submit.setVisibility(View.INVISIBLE);
+            bar.setVisibility(View.VISIBLE);
+            if(isEmailValid(txt_email.getText())){
+                if(!txt_name.getText().toString().equals("")){
+                    if(txt_pass.getText().toString().length()>=8){
+                        if(!spn_state.getSelectedItem().toString().equals("Select State")){
+                            if(!spn_city.getSelectedItem().toString().equals("Select City")){
+                                if(!spn_area.getSelectedItem().toString().equals("Select Area")){
+                                    if(!spn_crop.getSelectedItem().toString().equals("Select Crop")){
+                                        mAuth.createUserWithEmailAndPassword(txt_email.getText().toString(),txt_pass.getText().toString())
+                                                .addOnCompleteListener(task -> {
+                                                    if(task.isSuccessful()){
+                                                        Map<String,String> data = new HashMap<>();
+                                                        data.put("name",txt_name.getText().toString());
+                                                        data.put("email",txt_email.getText().toString());
+                                                        data.put("state",spn_state.getSelectedItem().toString());
+                                                        data.put("city",spn_city.getSelectedItem().toString());
+                                                        data.put("area",spn_area.getSelectedItem().toString());
+                                                        data.put("crop",spn_crop.getSelectedItem().toString());
+                                                        firebase.collection("user").document(txt_email.getText().toString()).set(data)
+                                                                .addOnCompleteListener(task12 -> {
+                                                                    if(task12.isSuccessful()){
+                                                                        Toast.makeText(Register.this, "Registration Complete", Toast.LENGTH_SHORT).show();
+                                                                        startActivity(new Intent(getApplicationContext(),MainPage.class));
+                                                                    }else{
+                                                                        Toast.makeText(Register.this, "Some Error Occurred!!", Toast.LENGTH_SHORT).show();
+                                                                        mAuth.signOut();
+                                                                        finish();
+                                                                    }
+                                                                });
+                                                    }else {
+                                                        Toast.makeText(Register.this, "Try Later", Toast.LENGTH_SHORT).show();
+                                                        finish();
+                                                    }
+                                                });
+                                    }else{
+                                        Toast.makeText(Register.this, "Select Crop!!", Toast.LENGTH_SHORT).show();
+                                        btn_submit.setEnabled(true);
+                                        bar.setVisibility(View.INVISIBLE);
+                                        btn_submit.setVisibility(View.VISIBLE);
+                                    }
+                                }else{
+                                    Toast.makeText(Register.this, "Select Area!!", Toast.LENGTH_SHORT).show();
+                                    btn_submit.setEnabled(true);
+                                    bar.setVisibility(View.INVISIBLE);
+                                    btn_submit.setVisibility(View.VISIBLE);
+                                }
+                            }else{
+                                Toast.makeText(Register.this, "Select City!!", Toast.LENGTH_SHORT).show();
+                                btn_submit.setEnabled(true);
+                                bar.setVisibility(View.INVISIBLE);
+                                btn_submit.setVisibility(View.VISIBLE);
+                            }
+                        }else{
+                            Toast.makeText(Register.this, "Select State!!", Toast.LENGTH_SHORT).show();
+                            btn_submit.setEnabled(true);
+                            bar.setVisibility(View.INVISIBLE);
+                            btn_submit.setVisibility(View.VISIBLE);
+                        }
+
+                    }else{
+                        Toast.makeText(Register.this, "Password is Too Short!!", Toast.LENGTH_SHORT).show();
+                        btn_submit.setEnabled(true);
+                        bar.setVisibility(View.INVISIBLE);
+                        btn_submit.setVisibility(View.VISIBLE);
+                    }
+                }else{
+                    Toast.makeText(Register.this, "Enter A Name !!", Toast.LENGTH_SHORT).show();
+                    btn_submit.setEnabled(true);
+                    bar.setVisibility(View.INVISIBLE);
+                    btn_submit.setVisibility(View.VISIBLE);
+                }
+            }else{
+                Toast.makeText(Register.this, "Enter A Valid Email!!", Toast.LENGTH_SHORT).show();
+                btn_submit.setEnabled(true);
+                bar.setVisibility(View.INVISIBLE);
+                btn_submit.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(),Login.class));
     }
 }
 

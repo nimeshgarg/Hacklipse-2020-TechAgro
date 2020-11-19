a45801c52a260aaf1226a0f92238ea364b85eed7
deepankarvarma <satwikdpshrit@gmail.com>
2020-11-19 13:00:54 +0530

    added add crop and pest

--- app/src/main/java/com/example/techargo/MyField.java
+++ app/src/main/java/com/example/techargo/MyField.java
@@ -2,7 +2,9 @@ package com.example.techargo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MyField extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MyField extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myfield);
+        Button btn_add_crop_my_field = findViewById(R.id.btn_add_crop_my_field);
+        Button btn_add_pest_attack_my_field = findViewById(R.id.btn_add_pest_attack_my_field);
+        btn_add_crop_my_field.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),AddCrop.class)));
+        btn_add_pest_attack_my_field.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),PestAttack.class)));
     }
 }
